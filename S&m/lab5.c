a4e1c61545b72ed3b2f45c6d20291d8ebfe8b0d5
Abhil-ekh <165144949+Abhil-ekh@users.noreply.github.com>
2026-05-21 09:46:58 +0545

    Add files via upload

---
+++ S&m/lab5.c
@@ -0,0 +1,20 @@
+#include <conio.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+  long int i, s, x, y, z, nd, seed;
+  int n;
+  seed = 61;
+  printf("How many random numbers to be generated: ");
+  scanf("%d", &n);
+  for (i = 1; i <= n; i++) {
+    s = (seed * seed);
+    y = s / 10.0;
+    z = y / 100.;
+    x = (int)((y / 100. - z) * 100.);
+    printf("x =  %d\n", x);
+    seed = x;
+    printf(" %4ld ", x);
+  }
+}
